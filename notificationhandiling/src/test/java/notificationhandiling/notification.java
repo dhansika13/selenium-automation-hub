3adf7252a81f180e0c5510964dfaf2034f84b319
dhansika13 <159477336+dhansika13@users.noreply.github.com>
2026-03-27 15:12:48 +0530

    Add files via upload

---
+++ notificationhandiling/src/test/java/notificationhandiling/notification.java
@@ -0,0 +1,23 @@
+package notificationhandiling;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class notification {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		options.addArguments("--incognito");
+		WebDriver driver=new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://demoapps.qspiders.com/ui/browserNot?sublist=0");
+		Thread.sleep(3000);
+		driver.findElement(By.id("browNotButton")).click();
+		
+		
+	}
+
+}
